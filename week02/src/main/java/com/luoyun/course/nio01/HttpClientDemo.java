b670559beea4d7cce54e48ea65138b05f6d8d2db
luoyun0711 <1187038675@qq.com>
2021-09-26 17:35:41 +0800

    第二周第6题

---
+++ week02/src/main/java/com/luoyun/course/nio01/HttpClientDemo.java
@@ -0,0 +1,46 @@
+package com.luoyun.course.nio01;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import javax.swing.text.html.parser.Entity;
+
+
+/**
+ * HttpClientDemo
+ *
+ * @author luoyun
+ * @data 2021/9/26
+ */
+public class HttpClientDemo {
+
+    public static CloseableHttpClient httpclient = HttpClients.createDefault();
+
+    public static void main(String[] args) {
+        CloseableHttpResponse response = null;
+        try {
+            HttpGet httpget = new HttpGet("http://localhost:8801");
+            response = httpclient.execute(httpget);
+            if (response == null || response.getEntity() == null){
+                System.out.println(response.toString());
+            }
+            System.out.println(EntityUtils.toString(response.getEntity()));
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if (response != null){
+                try {
+                    response.close();
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
+    }
+
+}
